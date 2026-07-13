03270d92243cb1201f7ba0153e04fafec9e5a284
Jonathan Krall <jonathan.krall@lexicon.se>
2026-07-13 13:31:37 +0200

    Add project files.

---
+++ LexiconUniversity2026.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LexiconUniversity2026.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
