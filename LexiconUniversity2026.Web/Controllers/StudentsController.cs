659d77adafb373e21f30784c53033e0e5e264d13
Jonathan Krall <jonathan.krall@lexicon.se>
2026-07-14 13:33:46 +0200

    Added StudentCreateViewModel and fixed Create-functionality

--- LexiconUniversity2026.Web/Controllers/StudentsController.cs
+++ LexiconUniversity2026.Web/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bogus;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -10,6 +11,7 @@ namespace LexiconUniversity2026.Web.Controllers
     public class StudentsController : Controller
     {
         private readonly LexiconUniversityContext _context;
+        private static Faker _faker; 
 
         public StudentsController(LexiconUniversityContext context)
         {
@@ -20,13 +22,15 @@ namespace LexiconUniversity2026.Web.Controllers
         public async Task<IActionResult> Index()
         {
             var model = _context.Students.AsNoTracking()
+                .OrderByDescending(s=>s.Id)
                 .Select(s => new StudentIndexViewModel
                 {
                     Id = s.Id,
                     Avatar = s.Avatar,
                     FullName = s.Name.FullName,
                     City = s.Address.City
-                });
+                })
+                .Take(5);
            
             return View(await model.ToListAsync());
         }
@@ -60,15 +64,24 @@ namespace LexiconUniversity2026.Web.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Avatar,FirstName,LastName,Email")] Student student)
+        public async Task<IActionResult> Create(StudentCreateViewModel viewModel)
         {
+            _faker = new Faker("sv");
+
             if (ModelState.IsValid)
             {
+                Student student = new Student()
+                {
+                    Avatar = _faker.Internet.Avatar(),
+                    Name = new Name() { FirstName = viewModel.FirstName, LastName = viewModel.LastName },
+                    Email = viewModel.Email,
+                    Address = new Address { Street = viewModel.Street, ZipCode = viewModel.ZipCode, City = viewModel.City }
+                };
                 _context.Add(student);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(student);
+            return View(viewModel);
         }
 
         // GET: Students/Edit/5
---
+++ LexiconUniversity2026.Web/Models/ViewModels/StudentCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace LexiconUniversity2026.Web.Models.ViewModels
+{
+    public class StudentCreateViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+    }
+}
